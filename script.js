2e0566f650aadf8810bf97f447566fc514aa3ab0
Ivan Trajanovski <ivantrajanovski@gmail.com>
2020-01-04 15:54:40 +0100

    just add html and css, no js yet

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operation, a, b) {
+    if(operation == add) {
+        return add(a, b);
+    } else if(operation == subtract) {
+        return subtract(a, b);
+    } else if(operation == multiply) {
+        return multiply(a, b);
+    } else if(operation == divide) {
+        return divide(a, b);
+    }
+}
