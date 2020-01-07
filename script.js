9193d92346eea2b44a222e6907497baa96544df2
Ivan Trajanovski <ivantrajanovski@gmail.com>
2020-01-07 01:31:58 +0100

    change table to grid view

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let display = document.getElementsByName("display");
+
 function add(a, b) {
     return a + b;
 }
@@ -25,3 +27,30 @@ function operate(operation, a, b) {
         return divide(a, b);
     }
 }
+
+//event listeners on numbers
+
+let numberButtons = document.getElementsByClassName("numberButton");
+
+for (let i=0; i<numberButtons.length; i++){
+    numberButtons[i].addEventListener(
+        "click", 
+        function(){
+        setNumbers(numberButtons[i].id);
+        }
+    );
+}
+
+//event listeners on operators
+
+let operators = document.getElementsByClassName("operator");
+
+for(let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener(
+        "click",
+        function() {
+            
+        }
+    )
+}
+
